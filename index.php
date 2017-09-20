66d44492e5446b33bcf48c6631c6cc8b4f3e099e
Nightblade73 <v-zar@list.ru>
2017-09-20 20:54:41 +0400

    лёгкие пробники php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
-<?
-phpinfo();
-
-?>
+<html>
+    <body>
+        <?php
+        $txt1 = "Привет, Мир!";
+        $txt2 = "Как дела?";
+        print "<p>" . $txt1 . "</p><p>" . $txt2 . "</p>";
+        ?>
+    </body>
+</html>
