a8fd90921228bac0eda56ef9650cecaa68be7df8
Nightblade73 <v-zar@list.ru>
2017-09-27 21:01:11 +0400

    Добавлены стили. Добавлены области с основной информацией. Менюшка зафиксирована и "плавае"т вместе с прокруткой колеса

--- index.php
+++ index.php
@@ -1,9 +1,35 @@
 <html>
+
+    <head>
+        <title>Блочная вёрстка</title>
+        <link rel="stylesheet" type="text/css" href="css\styles.css">
+    </head>
     <body>
-        <?php
-        $txt1 = "Привет, Мир!";
-        $txt2 = "Как дела?";
-        print "<p>" . $txt1 . "</p><p>" . $txt2 . "</p>";
-        ?>
+        <div id="container">
+            <div id="header" text='header (шапка сайта'">
+                <div id="navigation">
+                    <h2>Блок навигации</h2>
+                </div>
+                <h2>header (шапка сайта)</h2>
+            </div>
+
+            <!--	<div id="sidebar">
+                    <h2>Левая панель</h2>
+                    </div>-->
+
+            <div id="content">
+                <h2>Основной контент страницы</h2>
+            </div>
+
+            <div id="clear">
+
+            </div>
+
+            <div id="footer">
+                <h2>footer (низ сайта)</h2>
+            </div>
+
+        </div>
     </body>
 </html>
+
