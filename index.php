9fb15f98746a06c6a7e69747349425456ce4d0e0
Nightblade73 <v-zar@list.ru>
2017-10-01 18:26:22 +0400

    Добавление JS

--- index.php
+++ index.php
@@ -6,12 +6,33 @@
     <body>
         <div id="container">
             <div id="header">
-                <div id="navigation">
-                    <h2>Кракозября</h2>
+                <div id="main">
+                    <h2>
+                        <a href="" id="back-to-top">Главная</a>
+                    </h2>
                 </div>
-                <h2>конечно</h2>
+                <div id="aboutUs">
+                    <h2>
+                        <a href="" id="back-to-top">О нас</a>                      
+                    </h2>
+                </div>
+                <div id="examples">
+                    <h2>Примеры</h2>
+                </div>
+                <div id="contacts">
+                    <h2>Контакты</h2>
+                </div>
             </div>
-            <div id="content">
+            <div id="firstPage">
+                <h2>Основной контент страницы</h2>
+            </div>
+            <div id="secondPage">
+                <h2>Основной контент страницы</h2>
+            </div>
+            <div id="thirdPage">
+                <h2>Основной контент страницы</h2>
+            </div>
+            <div id="fourthPage">
                 <h2>Основной контент страницы</h2>
             </div>
             <div id="footer">
@@ -22,6 +43,29 @@
 
             </div>
         </div>
+        <script language="jquery">
+
+//                backToTop = function();
+//                $(window).on('scroll', function () {
+//                    backToTop();
+//                });
+                $('#back-to-top').on('click', function (e) {
+                    e.preventDefault();
+                    $('html,body').animate({
+                        scrollTop: 0
+                    }, 1500);
+                });
+            
+            $(document).bind('mousewheel', function (e) {
+                var nt = $(document.body).scrollTop() - (e.deltaY * e.deltaFactor * 500);
+                e.preventDefault();
+                e.stopPropagation();
+                $(document.body).stop().animate({
+                    scrollTop: nt
+                }, 500, 'easeInOutCubic');
+            });
+        </script>
     </body>
+
 </html>
 
