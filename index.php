b7d776729ff2d66ed1b5ec92ada9480eda7b82cb
Nightblade73 <v-zar@list.ru>
2017-10-01 12:32:21 +0400

    Настройка стилей основных блоков с информацией

--- index.php
+++ index.php
@@ -1,34 +1,26 @@
 <html>
-
     <head>
         <title>Блочная вёрстка</title>
         <link rel="stylesheet" type="text/css" href="css\styles.css">
     </head>
     <body>
         <div id="container">
-            <div id="header" text='header (шапка сайта'">
+            <div id="header">
                 <div id="navigation">
-                    <h2>Блок навигации</h2>
+                    <h2>Кракозября</h2>
                 </div>
-                <h2>header (шапка сайта)</h2>
+                <h2>конечно</h2>
             </div>
-
-            <!--	<div id="sidebar">
-                    <h2>Левая панель</h2>
-                    </div>-->
-
             <div id="content">
                 <h2>Основной контент страницы</h2>
+            </div>
+            <div id="footer">
+                <h2>footer (низ сайта)</h2>
             </div>
 
             <div id="clear">
 
             </div>
-
-            <div id="footer">
-                <h2>footer (низ сайта)</h2>
-            </div>
-
         </div>
     </body>
 </html>
