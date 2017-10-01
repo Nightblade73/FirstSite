79ec19f1c915e45fcdc4ecfdccf1e3d11d82f232
Nightblade73 <v-zar@list.ru>
2017-10-01 19:23:59 +0400

    Неработающий JS

--- index.php
+++ index.php
@@ -8,31 +8,37 @@
             <div id="header">
                 <div id="main">
                     <h2>
-                        <a href="" id="back-to-top">Главная</a>
+                        <a href="#firstPage">Главная</a>
                     </h2>
                 </div>
                 <div id="aboutUs">
                     <h2>
-                        <a href="" id="back-to-top">О нас</a>                      
+                        <a href="#secondPage">О нас</a>                      
                     </h2>
                 </div>
                 <div id="examples">
-                    <h2>Примеры</h2>
+                    <h2>
+                        <a href="#thirdPage">Примеры</a> 
+                    </h2>
                 </div>
                 <div id="contacts">
-                    <h2>Контакты</h2>
+                    <h2>
+                        <a href="#forthPage">Контакты</a>                       
+                    </h2>
                 </div>
             </div>
-            <div id="firstPage">
+            <div id="firstPage"> 
+                <section></section>
                 <h2>Основной контент страницы</h2>
             </div>
             <div id="secondPage">
+                <section></section>
                 <h2>Основной контент страницы</h2>
             </div>
             <div id="thirdPage">
                 <h2>Основной контент страницы</h2>
             </div>
-            <div id="fourthPage">
+            <div id="forthPage">
                 <h2>Основной контент страницы</h2>
             </div>
             <div id="footer">
@@ -43,26 +49,57 @@
 
             </div>
         </div>
-        <script language="jquery">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
 
 //                backToTop = function();
 //                $(window).on('scroll', function () {
 //                    backToTop();
 //                });
-                $('#back-to-top').on('click', function (e) {
-                    e.preventDefault();
-                    $('html,body').animate({
-                        scrollTop: 0
-                    }, 1500);
+//            $('#back-to-top').on('click', function (e) {
+//                e.preventDefault();
+//                $('html,body').animate({
+//                    scrollTop: 0
+//                }, 1500);
+//            });
+
+
+//            $(document).ready(function () {
+//                $("#header").on("click", "a", function (event) {
+//                    //отменяем стандартную обработку нажатия по ссылке
+//                    event.preventDefault();
+//                    //забираем идентификатор бока с атрибута href
+//                    var id = $(this).attr('href'),
+//                            //узнаем высоту от начала страницы до блока на который ссылается якорь
+//                    top = $(id).offset().top;
+//                    //анимируем переход на расстояние - top за 1500 мс
+//                    $('body,html').animate({scrollTop: top}, 1500);
+//                });
+//            });
+
+
+            $(document).ready(function () {
+                // Add smooth scrolling to all links
+                $("a").on('click', function (event) {
+
+                    // Make sure this.hash has a value before overriding default behavior
+                    if (this.hash !== "") {
+                        // Prevent default anchor click behavior
+                        event.preventDefault();
+
+                        // Store hash
+                        var hash = this.hash;
+
+                        // Using jQuery's animate() method to add smooth page scroll
+                        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+                        $('html, body').animate({
+                            scrollTop: $(hash).offset().top
+                        }, 800, function () {
+
+                            // Add hash (#) to URL when done scrolling (default click behavior)
+                            window.location.hash = hash;
+                        });
+                    } // End if
                 });
-            
-            $(document).bind('mousewheel', function (e) {
-                var nt = $(document.body).scrollTop() - (e.deltaY * e.deltaFactor * 500);
-                e.preventDefault();
-                e.stopPropagation();
-                $(document.body).stop().animate({
-                    scrollTop: nt
-                }, 500, 'easeInOutCubic');
             });
         </script>
     </body>
