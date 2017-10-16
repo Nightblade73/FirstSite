455d82ea22c100587e3e672cd4df786bcdba3b72
Nightblade73 <v-zar@list.ru>
2017-10-16 22:54:17 +0400

    Добавлены гифки, анимация для гивок, крч огонь

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
         <title>Представление фейерверков</title>
 
         <link rel="stylesheet" type="text/css" href="css\styles.css">
+        <link rel="stylesheet" type="text/css" href="css\gif.css">
     </head>
     <body>
         <div id="container">
@@ -18,17 +19,20 @@
                     <iframe width="854" height="480" src="https://www.youtube.com/embed/V_MoNkm0fl8" frameborder="0" allowfullscreen></iframe>
                 </div>  
                 <div id="description">
-                    <p>	
-                        Не следует, однако забывать, что реализация намеченных плановых заданий в значительной степени обуславливает создание существенных финансовых и административных условий. Значимость этих проблем настолько очевидна, что новая модель организационной деятельности в значительной степени обуславливает создание дальнейших направлений развития. Задача организации, в особенности же начало повседневной работы по формированию позиции позволяет оценить значение дальнейших направлений развития. 
-                    </p>
                     <p>
-                        Равным образом дальнейшее развитие различных форм деятельности обеспечивает широкому кругу (специалистов) участие в формировании позиций, занимаемых участниками в отношении поставленных задач. Значимость этих проблем настолько очевидна, что реализация намеченных плановых заданий представляет собой интересный эксперимент проверки систем массового участия. Не следует, однако забывать, что сложившаяся структура организации в значительной степени обуславливает создание модели развития.
+                        Однажды сюда загрузится текст из файла, но это только однажды
                     </p>
 
                 </div>             
             </div>
             <div id="moreExamples">
                 <a>Примеры работ</a>
+                <div class="gif-with-play">
+                    <img src="fireworks/1.jpg" alt="Self Portrait" data-srcgif="fireworks/1.gif">
+                </div>
+                <div class="gif-with-play">
+                    <img src="fireworks/2.jpg" alt="Self Portrait" data-srcgif="fireworks/2.gif">
+                </div>
             </div>
             <div id="doSale">
                 <a>Сделать заказ</a>
@@ -36,23 +40,13 @@
             <div id="contactsCompany">
                 <a>Контакты</a>
             </div>           
-        </div>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        </div>  
+        <script src="http://code.jquery.com/jquery-3.2.1.js"></script>
         <script>
-            <?php include("scrolling.js"); ?>  
-                $(document).ready(function () {
-    $("a").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $('html, body').animate({
-                scrollTop: ($(hash).offset().top) - 80
-            }, 1000, function () {
-                window.location.hash = hash;
-            });
-        }
-    });
-});
+<?php
+include("javascript/scrolling.js");
+include("javascript/gifAnimation.js");
+?>
         </script>
     </body>
 
---
+++ javascript/gifAnimation.js
@@ -0,0 +1,29 @@
+(function($) {
+ 
+  $('.gif-with-play').on('click', function() {
+ 
+    var $this   = $(this),        
+        $img    = $this.children('img'),
+        $imgSrc = $img.attr('src'),
+        $imgSrcgif = $img.attr('data-srcgif'),
+        $imgExt = $imgSrcgif.split('.');
+ 
+    if($imgExt[1] === 'gif') {
+      $this.addClass('loading-hide');
+ 
+      $img.attr('src', $img.data('srcgif')).attr('data-srcgif', $imgSrc);
+ 
+      $($img).load(function(){
+        $this.removeClass('loading-hide');
+      });   
+    } else {
+      $img.attr('src', $imgSrcgif).attr('data-srcgif', $img.data('srcgif'));
+    }
+ 
+    $this.toggleClass('play');
+ 
+  });
+ 
+})(jQuery);
+
+
