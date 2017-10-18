bb707ad0a8f53d2d6fc1c0bf270e5ab60d0d3faa
Nightblade73 <v-zar@list.ru>
2017-10-18 11:39:59 +0400

    кажется есть, но мне не нравится как это работает

--- index.php
+++ index.php
@@ -42,13 +42,29 @@
             </div>
             <div id="doSale">
                 <a>Сделать заказ</a>
-                <form>
-                    <p>Введите свое имя : </p>
-                    <input type="text" name="Person" class="textbox"/>
-                    <p>Введите свой e-mail : </p>
-                    <input type="text" name="Address" class="textbox"/> 
-                    <button class="button" name="Send">Отправить</button>
+                <form method="get" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                    <p name="Person">Введите свое имя :
+                        <input type="text" name="Person" class="textbox"/>
+                    </p>
+                    <p name="email" value="Отправить">Введите свой e-mail : 
+                        <input type="text" name="Address" id="email" class="textbox"/>      
+
+                        <?php
+                        if (isset($_GET["Address"])) {
+                            if (!filter_input(INPUT_GET, "Address", FILTER_VALIDATE_EMAIL) === false) {
+                                echo("Email is valid");
+                            } else {
+                                echo("Email is not valid");
+                            }
+                        }
+                        ?>
+
+                    </p> 
+                    <p>
+                        <input class="button" type ="submit" name ="Send"value="Отправить"> 
+                    </p>
                 </form>
+
             </div>
             <div id="contactsCompany">
                 <a>Контакты</a>
@@ -63,6 +79,34 @@
 include("javascript/scrolling.js");
 include("javascript/gifAnimation.js");
 ?>
+//            $('#email').blur(function () {
+//                var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,6}\.)?[a-z]{2,6}$/i;
+//                if ($(this).val() && !pattern.test($(this).val()) {
+//                    alert('Вы ввели некорректный e-mail');
+//                    $(this).focus();
+//                }
+//            });
+//            $(document).ready(function () {
+//                var pattern = /^[a-z0-9_-]+@[a-z0-9-]+\.[a-z]{2,6}$/i;
+//                var mail = $('#email');
+//                mail.blur(function () {
+//                    if (mail.val() !== '') {
+//                        if (mail.val().search(pattern) === 0) {
+//                            $('#valid').text('Подходит');
+//                            $('#submit').attr('disabled', false);
+//                            mail.removeClass('error').addClass('ok');
+//                        } else {
+//                            $('#valid').text('Не подходит');
+//                            $('#submit').attr('disabled', true);
+//                            mail.addClass('ok');
+//                        }
+//                    } else {
+//                        $('#valid').text('Поле e-mail не должно быть пустым!');
+//                        mail.addClass('error');
+//                        $('#submit').attr('disabled', true);
+//                    }
+//                });
+//            });
         </script>
     </body>
 
