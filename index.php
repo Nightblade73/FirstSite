375299cf1696b67088400214485597dee5769c05
Nightblade73 <v-zar@list.ru>
2017-10-24 19:27:35 +0400

    небольшие изменения

--- index.php
+++ index.php
@@ -8,7 +8,84 @@
     <script src="http://code.jquery.com/jquery-3.2.1.js"></script>
     <body>
         <div id="container">
-            <?php include("head.php"); ?>           
+            <?php
+            include("head.php");
+
+            class Logger {
+
+                //статические переменные
+                public static $PATH;
+                protected static $loggers = array();
+                protected $name;
+                protected $file;
+                protected $fp;
+
+                public function __construct($name, $file = null) {
+                    $this->name = $name;
+                    $this->file = $file;
+
+                    $this->open();
+                }
+
+                public function open() {
+                    if (self::$PATH == null) {
+                        return;
+                    }
+
+                    $this->fp = fopen($this->file == null ? self::$PATH . '/' . $this->name . '.log' : self::$PATH . '/' . $this->file, 'a+');
+                }
+
+                public static function getLogger($name = 'root', $file = null) {
+                    if (!isset(self::$loggers[$name])) {
+                        self::$loggers[$name] = new Logger($name, $file);
+                    }
+
+                    return self::$loggers[$name];
+                }
+
+                public function log($message) {
+                    if (!is_string($message)) {
+                        $this->logPrint($message);
+
+                        return;
+                    }
+
+                    $log = '';
+
+                    $log .= '[' . date('D M d H:i:s Y', time()) . '] ';
+                    if (func_num_args() > 1) {
+                        $params = func_get_args();
+
+                        $message = call_user_func_array('sprintf', $params);
+                    }
+
+                    $log .= $message;
+                    $log .= "\n";
+
+                    $this->_write($log);
+                }
+
+                public function logPrint($obj) {
+                    ob_start();
+
+                    print_r($obj);
+
+                    $ob = ob_get_clean();
+                    $this->log($ob);
+                }
+
+                protected function _write($string) {
+                    fwrite($this->fp, $string);
+
+                    echo $string;
+                }
+
+                public function __destruct() {
+                    fclose($this->fp);
+                }
+
+            }
+            ?>           
             <div id="mainPage"> 
                 <div class="infoBlock" id="infoblock"></div>
                 <div id="logo">
@@ -57,7 +134,7 @@
                             if (!filter_input(INPUT_GET, "Address", FILTER_VALIDATE_EMAIL) === false) {
                                 $var = 'Заказ оформлен на адрес:<br>' . $_GET["Address"];
                             } else {
-                                $var = 'Заказ не оформлен\nЭлектронный адрес не верный';
+                                $var = 'Заказ не оформлен<br>Электронный адрес не верный';
                             }
                             echo '<script>
                                 $(window).ready(function () {                             
