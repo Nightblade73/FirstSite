0ee537603f2e8f6971bc84d5d688434fa6e796ec
Nightblade73 <v-zar@list.ru>
2017-10-29 19:19:00 +0400

    Добавлены изображения. Настроена выгрузка из БД. Настроены стили для выгрузки из БД. Расширить количество выводимой информации очень легко

--- DBController.php
+++ DBController.php
@@ -11,8 +11,6 @@ class DBController {
         if (mysqli_connect_errno()) {
             echo 'Ошибка подключения к серверу MySQL. Код ошибки:' . mysqli_connect_error();
             exit;
-        } else {
-            echo 'Подключение прошло успешно ' . $GLOBALS['mysqli']->host_info;
         }
     }
 
@@ -21,18 +19,70 @@ class DBController {
         echo 'Отключение прошло успешно';
     }
 
-    function getInformationById(int $id) {
-        $sqlStr = 'SELECT F.name, F.price, D.description_text, D.shoot_count, C.caliber_num, D.time
+//    function getInformationById(int $id) {
+//        $sqlStr = 'SELECT F.name, F.price, D.description_text, D.shoot_count, C.caliber_num, D.time
+//                    FROM firework F JOIN description D
+//                    ON F.id_desc = D.id_desc
+//                    JOIN caliber C
+//                    ON D.id_caliber = C.id_caliber
+//                    WHERE id_firework = ' . $id . ';';
+//
+//        $res = $GLOBALS['mysqli']->query($sqlStr);
+//        $res->data_seek(0);
+//        $row = $res->fetch_assoc();
+//        echo '<div class="element">';
+//        echo '<img src="images/' . $id . '.jpg">';
+//        echo '<p name="Name">' . $row['name'] . '</p>';
+//        echo '<p>Цена: ' . $row['price'] . ' р.</p>';
+//        echo '<p>Описание: ' . $row['description_text'] . '</p>';
+//        echo '<p>Количество выстрелов: ' . $row['shoot_count'] . ' шт.</p>';
+//        echo '<p>Калибр: ' . $row['caliber_num'] . '</p>';
+//        echo '<p>Время: до ' . $row['time'] . ' секунд</p>';
+//        echo '</div>';
+//    }
+
+    function getAllInfo() {
+
+        function getInformationById(int $id) {
+            $sqlStr = 'SELECT F.name, F.price, D.description_text, D.shoot_count, C.caliber_num, D.time
                     FROM firework F JOIN description D
                     ON F.id_desc = D.id_desc
                     JOIN caliber C
                     ON D.id_caliber = C.id_caliber
-                    WHERE id_firework=' . $id;
-        $res = $GLOBALS['mysqli']->query($sqlStr);
-        $res->data_seek(0);
-        while ($row = $res->fetch_assoc()) {
-            echo "<br> id = " . $row['F.name'];
+                    WHERE id_firework = ' . $id . ';';
+
+            $res = $GLOBALS['mysqli']->query($sqlStr);
+            $res->data_seek(0);
+            $row = $res->fetch_assoc();
+            echo '<div class="element">';
+            echo '<img src="images/' . $id . '.jpg">';
+            echo '<p name="Name">' . $row['name'] . '</p>';
+            echo '<p>Цена: ' . $row['price'] . ' р.</p>';
+            if (!empty($row['description_text'])) {
+                echo '<p>Описание: ' . $row['description_text'] . '</p>';
+            } else {
+                echo '<p>Описание: Описание отсутствует</p>';
+            }
+            echo '<p>Количество выстрелов: ' . $row['shoot_count'] . ' шт.</p>';
+            echo '<p>Калибр: ' . $row['caliber_num'] . '</p>';
+            echo '<p>Время: до ' . $row['time'] . ' секунд</p>';
+            echo '</div>';
         }
+
+        getInformationById(12001);
+        getInformationById(506);
+        getInformationById(851);
+        getInformationById(497);
+        getInformationById(852);
+        getInformationById(706);
+        getInformationById(8460);
+        getInformationById(720);
+        getInformationById(707);
+        getInformationById(610);
+        getInformationById(340);
+        getInformationById(342);
+        getInformationById(502);
+        getInformationById(7064);
     }
 
 }
