cb90b065033cda04a63d99bd87ce11c93e9b0228
Nightblade73 <v-zar@list.ru>
2017-11-03 08:57:34 +0400

    адаптивная менюшка готова

--- index.php
+++ index.php
@@ -12,36 +12,66 @@
         <![endif]-->
     </head>
     <body>
-        <div class="container">
-            <header>
-                <nav class="navbar navbar-default navbar-fixed-top menu">
-                    <div class="container">  
-                        <div class="navbar-header">
-                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                                <span class="sr-only">Toggle navigation</span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                            </button>
-                        </div>
 
-                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                            <ul class="nav navbar-nav links">
-                                <li class="active-link"><a href="#mainPage">главная</a></li>
-                                <li><a href="#aboutCompany">о нас</a></li>
-                                <li><a href="#moreExamples">примеры</a></li>
-                                <li><a href="#doSale">сделать заказ</a></li>
-                                <li><a href="#contactsCompany">контакты</a></li>
-
-                            </ul>
-                        </div>
+        <header>
+            <nav class="navbar navbar-default navbar-fixed-top menu">
+                <div class="container">  
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
                     </div>
-                </nav>
-            </header>
-        </div>
+						
+                    <div class="collapse navbar-collapse rows" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav links">
+                            <li class="active-link"><a href="#mainPage">главная</a></li>
+                            <li><a href="#aboutCompany">о нас</a></li>
+                            <li><a href="#moreExamples">примеры</a></li>
+                            <li><a href="#doSale">сделать заказ</a></li>
+                            <li><a href="#contactsCompany">контакты</a></li>
 
-        <script src="http://code.jquery.com/jquery.min.js"></script> 
-        <script src="js/bootstrap.min.js"></script> 
-    </body>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+        <div class="container">
+            <div class="part" id="mainPage"> 
+                <!--<div class="infoBlock" id="infoblock"></div>-->
+                <div class="logo">
+                    <a>Фейеверк компани</a>
+                </div>              
+            </div>
+            <div class="part" id="aboutCompany"> 
+                <div class="logo">
+                    <a>Фейеверк компани</a>
+                </div>              
+            </div><div class="part" id="moreExamples"> 
+                <div class="logo">
+                    <a>Фейеверк компани</a>
+                </div>              
+            </div><div class="part" id="doSale"> 
+                <div class="logo">
+                    <a>Фейеверк компани</a>
+                </div>              
+            </div><div class="part" id="contactsCompany"> 
+                <div class="logo">
+                    <a>Фейеверк компани</a>
+                </div>              
+            </div>              
+        </div>
+    </div>
+
+    <script src="http://code.jquery.com/jquery.min.js"></script> 
+    <script src="js/bootstrap.min.js"></script> 
+    <script>
+        <?php
+        include("javascript/scrolling.js");
+        ?>
+    </script>
+</body>
 
 </html>
